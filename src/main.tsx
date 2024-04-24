59ed0e5c1ff189197047eedd16f07f020550f602
Yogesh Navghane <124075039+yogeshNavghane67@users.noreply.github.com>
2024-04-25 02:00:05 +0530

    create user request

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import './global.css'
-import { BrowserRouter as Router} from "react-router-dom";
-import AppRoutes from './AppRoutes';
-import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate';
+import "./global.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./AppRoutes";
+import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
-      <Auth0ProviderWithNavigate >
-      <AppRoutes/>
-      </Auth0ProviderWithNavigate>
+      <QueryClientProvider client={queryClient}>
+        <Auth0ProviderWithNavigate>
+          <AppRoutes />
+        </Auth0ProviderWithNavigate>
+      </QueryClientProvider>
     </Router>
   </React.StrictMode>
 );
