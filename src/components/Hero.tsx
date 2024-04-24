e1bf8ee443733ad366468dcc77f61daf796611e0
Yogesh Navghane <124075039+yogeshNavghane67@users.noreply.github.com>
2024-04-24 23:22:53 +0530

    inital commit

---
+++ src/components/Hero.tsx
@@ -0,0 +1,9 @@
+import hero from "../assets/hero.png";
+
+const Hero = () => {
+  return (
+    <div><img src={hero} className="w-full mx-h-[600px] object-cover"/></div>
+  )
+}
+
+export default Hero
---
+++ src/components/MainNav.tsx
@@ -0,0 +1,24 @@
+import UsernameMenu from "./UsernameMenu";
+import { Button } from "./ui/button";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const MainNav = () => {
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
+  return (
+    <span className="flex space-x-2 items-center">
+      {isAuthenticated ? (
+        <UsernameMenu />
+      ) : (
+        <Button
+          variant="ghost"
+          className="font-bold hover:text-orange-500 hover:bg-white"
+          onClick={async () => await loginWithRedirect()}
+        >
+          Log In
+        </Button>
+      )}
+    </span>
+  );
+};
+
+export default MainNav;
