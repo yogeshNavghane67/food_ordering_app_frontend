d0b5efa196c552d31421b6eae156ffb1e8f46243
Yogesh Navghane <124075039+yogeshNavghane67@users.noreply.github.com>
2024-04-25 19:41:13 +0530

    user profile api

--- src/auth/Auth0ProviderWithNavigate.tsx
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,5 +1,6 @@
-import { useCreateMyUser } from "@/api/MyUserApi";
-import { AppState, Auth0Provider } from "@auth0/auth0-react";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
+
 
 
 type Props ={
@@ -7,21 +8,18 @@ type Props ={
 }
 
 const Auth0ProviderWithNavigate = ({children}: Props) => {
-  const {createUser} = useCreateMyUser();
-
+const navigate = useNavigate();
  const domain = import.meta.env.VITE_AUTH0_DOMAIN;
  const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
  const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
-// const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
+const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
-if (!domain || !clientId || !redirectUri) {
+if (!domain || !clientId || !redirectUri || !audience) {
     throw new Error("unable to initialise auth");
   }
 
-  const onRedirectCallback = (appState?: AppState, user?: User) => {
-    if(user?.sub && user?.email) {
-      createUser({auth0Id: user.sub, email: user.email})
-    }
+  const onRedirectCallback = () => {
+    navigate("/auth-callback")
   };
 
   return (
@@ -30,6 +28,7 @@ if (!domain || !clientId || !redirectUri) {
       clientId={clientId}
       authorizationParams={{
         redirect_uri: redirectUri,
+        audience,
       }}
       onRedirectCallback={onRedirectCallback}
     >
