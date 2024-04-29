46e7d4abafb78e7850a14c96bf5162f2e791eb2e
Yogesh Navghane <124075039+yogeshNavghane67@users.noreply.github.com>
2024-04-29 23:42:21 +0530

    restaurant form and cuisines checkbox

--- src/components/UsernameMenu.tsx
+++ src/components/UsernameMenu.tsx
@@ -1,31 +1,46 @@
-import { useAuth0 } from '@auth0/auth0-react';
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from './ui/dropdown-menu'
-import { CircleUserRound } from 'lucide-react'
-import { Link } from 'react-router-dom';
-import { Separator } from './ui/separator';
-import { Button } from './ui/button';
+import { useAuth0 } from "@auth0/auth0-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { CircleUserRound } from "lucide-react";
+import { Link } from "react-router-dom";
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
 
 const UsernameMenu = () => {
-    const { user, logout } = useAuth0();
+  const { user, logout } = useAuth0();
   return (
     <DropdownMenu>
-        <DropdownMenuTrigger className='flex items-center px-3 font-bold hover:text-orange-500 gap-2'>
-            <CircleUserRound className="text-orange-500"/>
-            {user?.email}
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-            <DropdownMenuItem>
-            <Link to="/user-profile" className='font-bold hover:text-orange-400'>
-                User Profile
-            </Link>
-            </DropdownMenuItem>
-            <Separator/>
-            <DropdownMenuItem>
-                <Button onClick={() => logout()} className='flex flex-1 font-bold bg-orange-500'>Log Out</Button>
-            </DropdownMenuItem>
-        </DropdownMenuContent>
+      <DropdownMenuTrigger className="flex items-center px-3 font-bold hover:text-orange-500 gap-2">
+        <CircleUserRound className="text-orange-500" />
+        {user?.email}
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem>
+          <Link to="/manage-restaurant" className="font-bold hover:text-orange-400">
+            Manage Restaurant
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Link to="/user-profile" className="font-bold hover:text-orange-400">
+            User Profile
+          </Link>
+        </DropdownMenuItem>
+        <Separator />
+        <DropdownMenuItem>
+          <Button
+            onClick={() => logout()}
+            className="flex flex-1 font-bold bg-orange-500"
+          >
+            Log Out
+          </Button>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
-  )
-}
+  );
+};
 
-export default UsernameMenu
+export default UsernameMenu;
---
+++ src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -0,0 +1,73 @@
+import { Form } from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import DetailsSection from "./DetailsSection";
+import { Separator } from "@/components/ui/separator";
+import CuisinesSection from "./CuisinesSection";
+
+const formSchema = z.object({
+    restaurantName: z.string({
+        required_error: "restuarant name is required",
+      }),
+      city: z.string({
+        required_error: "city is required",
+      }),
+      country: z.string({
+        required_error: "country is required",
+      }),
+      deliveryPrice: z.coerce.number({
+        required_error: "delivery price is required",
+        invalid_type_error: "must be a valid number",
+      }),
+      estimatedDeliveryTime: z.coerce.number({
+        required_error: "estimated delivery time is required",
+        invalid_type_error: "must be a valid number",
+      }),
+      cuisines: z.array(z.string()).nonempty({
+        message: "please select at least one item",
+      }),
+      menuItems: z.array(
+        z.object({
+          name: z.string().min(1, "name is required"),
+          price: z.coerce.number().min(1, "price is required"),
+        })
+      ),
+    //   imageUrl: z.string().optional(),
+      imageFile: z.instanceof(File, { message: "image is required" }).optional(),
+});
+
+type restaurantFormData = z.infer<typeof formSchema>
+
+type Props = {
+    onSave: (restaurantFormData: FormData) => void;
+    isLoading: boolean;
+}
+
+const ManageRestaurantForm = ({ onSave, isLoading}: Props) => {
+    const form = useForm<restaurantFormData>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            cuisines: [],
+            menuItems: [{ name: "", price: 0}],
+        },
+    });
+
+    const onSubmit = (formDataJson: restaurantFormData) => {
+        // Todo - convert formDataJson to a new FromData object
+    }
+
+    return(
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="Space-y-8 bg-gray-50 p-10 rounded-lg">
+                <DetailsSection />
+                <br/>
+                <Separator />
+                <br/>
+                <CuisinesSection/>
+            </form>
+        </Form>
+    )
+};
+
+export default ManageRestaurantForm;
