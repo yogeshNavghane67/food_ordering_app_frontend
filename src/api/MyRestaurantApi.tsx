a6814e7cbcece494fc7f05039dc56111756c4b0d
Yogesh Navghane <124075039+yogeshNavghane67@users.noreply.github.com>
2024-05-01 01:27:02 +0530

    update my restaurant api request

--- src/api/MyRestaurantApi.tsx
+++ src/api/MyRestaurantApi.tsx
@@ -11,18 +11,18 @@ export const useGetMyRestaurant = () => {
   const getMyRestaurantRequest = async (): Promise<Restaurant> => {
     const accessToken = await getAccessTokenSilently();
 
-    const response =await fetch(`${API_BASE_URL}/api/my/restaurant`, {
+    const response = await fetch(`${API_BASE_URL}/api/my/restaurant`, {
       method: "GET",
       headers: {
-        Authorization: `Bearer ${accessToken}`
+        Authorization: `Bearer ${accessToken}`,
       },
     });
 
-    if(!response.ok) {
+    if (!response.ok) {
       throw new Error("Failed to get restaurant");
     }
     return response.json();
-  }
+  };
 
   const { data: restaurant, isLoading } = useQuery(
     "fetchMyRestaurant",
@@ -30,48 +30,86 @@ export const useGetMyRestaurant = () => {
   );
 
   return { restaurant, isLoading };
-
-}
+};
 
 export const useCreateMyRestaurant = () => {
-    const {getAccessTokenSilently } = useAuth0();
+  const { getAccessTokenSilently } = useAuth0();
 
-    const createMyRestaurantRequest = async(restaurantFormData: FormData):Promise<Restaurant> => {
-        const accessToken = await getAccessTokenSilently();
+  const createMyRestaurantRequest = async (
+    restaurantFormData: FormData
+  ): Promise<Restaurant> => {
+    const accessToken = await getAccessTokenSilently();
 
-        const response = await fetch(`${API_BASE_URL}/api/my/restaurant`, {
-            method: "POST",
-            headers: {
-              Authorization: `Bearer ${accessToken}`,
-            },
-            body: restaurantFormData,
-          });
+    const response = await fetch(`${API_BASE_URL}/api/my/restaurant`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: restaurantFormData,
+    });
 
-          if (!response.ok) {
-            throw new Error("Failed to create restaurant");
-          }
-      
-          return response.json();
+    if (!response.ok) {
+      throw new Error("Failed to create restaurant");
+    }
 
+    return response.json();
+  };
 
+  const {
+    mutate: createRestaurant,
+    isLoading,
+    isSuccess,
+    error,
+  } = useMutation(createMyRestaurantRequest);
 
-    };
+  if (isSuccess) {
+    toast.success("Restaurant created!");
+  }
 
-    const {
-        mutate: createRestaurant,
-        isLoading,
-        isSuccess,
-        error,
-      } = useMutation(createMyRestaurantRequest);
-    
-      if (isSuccess) {
-        toast.success("Restaurant created!");
-      }
-    
-      if (error) {
-        toast.error("Unable to update restaurant");
-      }
-    
-      return { createRestaurant, isLoading };
-    
-}
+  if (error) {
+    toast.error("Unable to update restaurant");
+  }
+
+  return { createRestaurant, isLoading };
+};
+
+export const useUpdateMyRestaurant = () => {
+  const { getAccessTokenSilently } = useAuth0();
+
+  const updateRestaurantRequest = async (
+    restaurantFormData: FormData
+  ): Promise<Restaurant> => {
+    const accessToken = await getAccessTokenSilently();
+
+    const response = await fetch(`${API_BASE_URL}/api/my/restaurant`, {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: restaurantFormData,
+    });
+
+    if (!response) {
+      throw new Error("Failed to update restaurant");
+    }
+
+    return response.json();
+  };
+
+  const {
+    mutate: updateRestaurant,
+    isLoading,
+    error,
+    isSuccess,
+  } = useMutation(updateRestaurantRequest);
+
+  if (isSuccess) {
+    toast.success("Restaurant Updated");
+  }
+
+  if (error) {
+    toast.error("Unable to update restaurant");
+  }
+
+  return { updateRestaurant, isLoading };
+};
--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,12 +1,23 @@
-import { useCreateMyRestaurant, useGetMyRestaurant } from '@/api/MyRestaurantApi'
-import ManageRestaurantForm from '@/forms/manage-restaurant-form/ManageRestaurantForm'
+import {
+  useCreateMyRestaurant,
+  useGetMyRestaurant,
+  useUpdateMyRestaurant,
+} from "@/api/MyRestaurantApi";
+import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
 
 const ManageRestaurantPage = () => {
-  const {createRestaurant, isLoading } = useCreateMyRestaurant();
-  const {restaurant} = useGetMyRestaurant();
+  const { createRestaurant, isLoading: isCreateLoading } = useCreateMyRestaurant();
+  const { restaurant } = useGetMyRestaurant();
+  const {updateRestaurant, isLoading: isUpdateLoading } = useUpdateMyRestaurant();
+
+  const isEditing = !!restaurant;
   return (
-    <ManageRestaurantForm restaurant={restaurant} onSave={createRestaurant} isLoading={isLoading}/>
-  )
-}
+    <ManageRestaurantForm
+      restaurant={restaurant}
+      onSave={isEditing ? updateRestaurant : createRestaurant}
+      isLoading={isCreateLoading || isUpdateLoading}
+    />
+  );
+};
 
 export default ManageRestaurantPage;
