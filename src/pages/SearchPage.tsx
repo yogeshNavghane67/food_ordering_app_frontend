7cd22789a8a6fe37b6102a30015ca8e4c458c69b
Yogesh Navghane <yogeshnavghane661@gmail.com>
2024-05-07 00:17:58 +0530

    cuisines filter

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,14 +1,17 @@
 import { useSearchRestaurants } from "@/api/RestaurantApi";
 import PaginationSelector from "@/components/PaginationSelector";
+
 import SearchBar, { SearchForm } from "@/components/SearchBar";
 import SearchResultCard from "@/components/SearchResultCard";
 import SearchResultInfo from "@/components/SearchResultInfo";
+import CuisineFilter from "@/components/CuisineFilter";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 
 export type SearchState = {
   searchQuery: string;
   page: number;
+  selectedCuisines: string[];
 };
 
 const SearchPage = () => {
@@ -16,8 +19,20 @@ const SearchPage = () => {
   const [searchState, setSearchState] = useState<SearchState>({
     searchQuery: "",
     page: 1,
+    selectedCuisines: [],
   });
+
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
+
   const { results, isLoading } = useSearchRestaurants(searchState, city);
+
+  const setSelectedCuisines = (selectedCuisines: string[]) => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      selectedCuisines,
+      page: 1,
+    }));
+  };
 
   const setPage = (page: number) => {
     setSearchState((prevState)=> ({
@@ -50,7 +65,14 @@ const SearchPage = () => {
   }
   return (
     <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
-      <div id="cuisines-list">insert cuisines here :)</div>
+      <div id="cuisines-list"><CuisineFilter
+          selectedCuisines={searchState.selectedCuisines}
+          onChange={setSelectedCuisines}
+          isExpanded={isExpanded}
+          onExpandedClick={() =>
+            setIsExpanded((prevIsExpanded) => !prevIsExpanded)
+          }
+        /></div>
       <div id="main-content" className="flex flex-col gap-5">
         <SearchBar
           searchQuery={searchState.searchQuery}
