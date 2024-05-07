5c24c64cbae5b8527a8759736e1249170bbb8f2e
Yogesh Navghane <yogeshnavghane661@gmail.com>
2024-05-08 01:28:08 +0530

    Detail page & menu items

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -6,6 +6,7 @@ import UserProfilePage from "./pages/UserProfilePage";
 import ProtectedRoute from "./auth/ProtectedRoute";
 import ManageRestaurantPage from "./pages/ManageRestaurantPage";
 import SearchPage from "./pages/SearchPage";
+import DetailPage from "./pages/DetailPage";
 
 const AppRoutes = () => {
   return (
@@ -20,7 +21,23 @@ const AppRoutes = () => {
       />
       <Route path="/auth-callback" element={<AuthCallbackPage />} />
 
-      <Route path="/search/:city" element={<Layout showHero={false}><SearchPage/></Layout>}/>
+      <Route
+        path="/search/:city"
+        element={
+          <Layout showHero={false}>
+            <SearchPage />
+          </Layout>
+        }
+      />
+
+      <Route
+        path="/detail/:restaurantId"
+        element={
+          <Layout showHero={false}>
+            <DetailPage />
+          </Layout>
+        }
+      />
 
       <Route element={<ProtectedRoute />}>
         <Route
---
+++ src/components/MenuItems.tsx
@@ -0,0 +1,22 @@
+
+import { MenuItem } from "@/types";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+
+type Props = {
+  menuItem: MenuItem;
+};
+
+const MenuItems = ({ menuItem }: Props) => {
+  return (
+    <Card className="cursor-pointer">
+      <CardHeader>
+        <CardTitle>{menuItem.name}</CardTitle>
+      </CardHeader>
+      <CardContent className="font-bold">
+        ₹{(menuItem.price)}
+      </CardContent>
+    </Card>
+  );
+};
+
+export default MenuItems;
