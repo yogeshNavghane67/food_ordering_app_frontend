d13fc652f4a564ae605353c8a3d075205efb9bb6
Yogesh Navghane <yogeshnavghane661@gmail.com>
2024-05-09 01:29:47 +0530

    conform delivery details & update user profile form

--- src/forms/user-profile-form/UserProfileForm.tsx
+++ src/forms/user-profile-form/UserProfileForm.tsx
@@ -25,15 +25,17 @@ const formSchema = z.object({
   country: z.string().min(1, "Country is required"),
 });
 
-type UserFormData = z.infer<typeof formSchema>;
+export type UserFormData = z.infer<typeof formSchema>;
 
 type Props = {
   currentUser: User;
   onSave: (userProfileData: UserFormData) => void;
   isLoading: boolean;
+  title?: string;
+  buttonText?: string;
 };
 
-const UserProfileForm = ({ onSave, isLoading, currentUser}: Props) => {
+const UserProfileForm = ({ onSave, isLoading, currentUser, title = "User Profile", buttonText = "Submit",}: Props) => {
   const form = useForm<UserFormData>({
     resolver: zodResolver(formSchema),
     defaultValues: currentUser,
@@ -50,7 +52,7 @@ const UserProfileForm = ({ onSave, isLoading, currentUser}: Props) => {
         className="space-y-4 bg-gray-50 rounded-lg md:p-10"
       >
         <div>
-          <h2 className="text-2xl font-bold">User Profile Form</h2>
+          <h2 className="text-2xl font-bold">{title}</h2>
           <FormDescription>
             View and change your profile information here
           </FormDescription>
@@ -128,7 +130,7 @@ const UserProfileForm = ({ onSave, isLoading, currentUser}: Props) => {
           <LoadingButton />
         ) : (
           <Button type="submit" className="bg-orange-500">
-            Submit
+            {buttonText}
           </Button>
         )}
       </form>
--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -8,6 +8,7 @@ import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { MenuItem as MenuItemType } from "@/types";
 import CheckoutButton from "@/components/CheckoutButton";
+import { UserFormData } from "@/forms/user-profile-form/UserProfileForm";
 
 export type CartItem = {
   _id: string;
@@ -77,6 +78,10 @@ const DetailPage = () => {
     });
   };
 
+  const onCheckout = (userFormData: UserFormData) => {
+    console.log("userFormData", userFormData)
+  }
+
   if (isLoading || !restaurant) {
     return "Loading...";
   }
@@ -108,6 +113,8 @@ const DetailPage = () => {
             />
             <CardFooter>
               <CheckoutButton
+                              disabled={cartItems.length === 0}
+                              onCheckout={onCheckout}
               />
             </CardFooter>
           </Card>
