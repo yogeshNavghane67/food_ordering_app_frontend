ec99b9a4815d9bf4a24e6ab18055aafab96aa254
Yogesh Navghane <yogeshnavghane661@gmail.com>
2024-05-10 02:18:42 +0530

    redirect to stripe

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -9,6 +9,7 @@ import { useParams } from "react-router-dom";
 import { MenuItem as MenuItemType } from "@/types";
 import CheckoutButton from "@/components/CheckoutButton";
 import { UserFormData } from "@/forms/user-profile-form/UserProfileForm";
+import { useCreateCheckoutSession } from "@/api/OrderApi";
 
 export type CartItem = {
   _id: string;
@@ -17,10 +18,11 @@ export type CartItem = {
   quantity: number;
 };
 
-
 const DetailPage = () => {
   const { restaurantId } = useParams();
   const { restaurant, isLoading } = useGetRestaurant(restaurantId);
+  const { createCheckoutSession, isLoading: isCheckoutLoading } =
+    useCreateCheckoutSession();
 
   const [cartItems, setCartItems] = useState<CartItem[]>(() => {
     const storedCartItems = sessionStorage.getItem(`cartItems-${restaurantId}`);
@@ -59,9 +61,8 @@ const DetailPage = () => {
       );
 
       return updatedCartItems;
-
-    })
-  }
+    });
+  };
 
   const removeFromCart = (cartItem: CartItem) => {
     setCartItems((prevCartItems) => {
@@ -78,9 +79,31 @@ const DetailPage = () => {
     });
   };
 
-  const onCheckout = (userFormData: UserFormData) => {
-    console.log("userFormData", userFormData)
-  }
+  const onCheckout = async (userFormData: UserFormData) => {
+    if (!restaurant) {
+      return;
+    }
+
+    const checkoutData = {
+      cartItems: cartItems.map((cartItem) => ({
+        menuItemId: cartItem._id,
+        name: cartItem.name,
+        quantity: cartItem.quantity.toString(),
+      })),
+      restaurantId: restaurant._id,
+      deliveryDetails: {
+        name: userFormData.name,
+        addressLine1: userFormData.addressLine1,
+        city: userFormData.city,
+        country: userFormData.country,
+        email: userFormData.email as string,
+      },
+    };
+
+    const data = await createCheckoutSession(checkoutData);
+    window.location.href = data.url;
+
+  };
 
   if (isLoading || !restaurant) {
     return "Loading...";
@@ -99,22 +122,25 @@ const DetailPage = () => {
           <RestaurantInfo restaurant={restaurant} />
           <span className="text-2xl font-bold tracking-tight">Menu</span>
           {restaurant.menuItems.map((menuItem) => (
-            <MenuItems menuItem={menuItem} addToCard={() => addToCart(menuItem)}
+            <MenuItems
+              menuItem={menuItem}
+              addToCard={() => addToCart(menuItem)}
             />
           ))}
         </div>
 
         <div>
           <Card>
-          <OrderSummary
+            <OrderSummary
               restaurant={restaurant}
               cartItems={cartItems}
               removeFromCart={removeFromCart}
             />
             <CardFooter>
               <CheckoutButton
-                              disabled={cartItems.length === 0}
-                              onCheckout={onCheckout}
+                disabled={cartItems.length === 0}
+                onCheckout={onCheckout}
+                isLoading={isCheckoutLoading}
               />
             </CardFooter>
           </Card>
