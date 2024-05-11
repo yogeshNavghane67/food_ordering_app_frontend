f26536378116707654bb05f920cbd0b528b49c76
Yogesh Navghane <yogeshnavghane661@gmail.com>
2024-05-12 01:36:09 +0530

    order status config

--- src/components/OrderStatusHeader.tsx
+++ src/components/OrderStatusHeader.tsx
@@ -1,5 +1,6 @@
 import { Order } from "@/types";
 import { Progress } from "./ui/progress";
+import { ORDER_STATUS } from "@/config/order-status-config";
 
 type Props = {
     order: Order;
@@ -22,16 +23,22 @@ type Props = {
         return `${hours}:${paddedMinutes}`;
       };
 
+      const getOrderStatusInfo = () => {
+        return (
+          ORDER_STATUS.find((o) => o.value === order.status) || ORDER_STATUS[0]
+        );
+      };
+
 
     return (
         <>
       <h1 className="text-4xl font-bold tracking-tighter flex flex-col gap-5 md:flex-row md:justify-between">
-        <span> Order Status: {order.status}</span>
+        <span> Order Status: {getOrderStatusInfo().label}</span>
         <span> Expected by: {getExpectedDelivery()}</span>
       </h1>
       <Progress
         className="animate-pulse"
-        value={50}
+        value={getOrderStatusInfo().progressValue}
       />
     </>
     )
