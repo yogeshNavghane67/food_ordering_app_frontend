7519da6ffb3f16f2080dcc72b8a38c61de4a5b8d
Yogesh Navghane <yogeshnavghane661@gmail.com>
2024-05-21 02:53:38 +0530

    21/5/2024

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,7 @@
 import landing2Image from "../assets/landing2.jpg";
 import landing3Image from "../assets/landing3.jpg";
+import mobile1Image from "../assets/mobile1.jpg";
+import mobile2Image from "../assets/mobile2.jpg";
 import SearchBar, { SearchForm } from "@/components/SearchBar";
 import { useNavigate } from "react-router-dom";
 
@@ -26,26 +28,33 @@ const HomePage = () => {
       <div className="grid md:grid-cols-2 gap-5">
         <img src={landing2Image} />
         <div className="flex flex-col items-center justify-center gap-4 text-center">
-          <span className="font-bold text-3xl tracking-tighter">
-          Seasonal Menus
+          <span className="font-bold text-2xl tracking-tighter">
+            Seasonal Menus
           </span>
           <span>
-          Each dish have a detailed description that includes <br/>
-          ingredients, cooking techniques, and flavor profiles
+            Each dish have a detailed description that includes <br />
+            ingredients, cooking techniques, and flavor profiles
           </span>
         </div>
       </div>
       <div className="grid md:grid-cols-2 gap-5">
         <img src={landing3Image} />
         <div className="flex flex-col items-center justify-center gap-4 text-center">
-          <span className="font-bold text-3xl tracking-tighter">
-          Authentic Ingredients
+          <span className="font-bold text-2xl tracking-tighter">
+            Authentic Ingredients
           </span>
           <span>
-          Highlight any authentic or hard-to-find ingredients <br/>
-          that add to the authenticity of the dishes
+            Highlight any authentic or hard-to-find ingredients <br />
+            that add to the authenticity of the dishes
           </span>
         </div>
+      </div>
+      <span className="text-red-600 font-bold tracking-tighter text-center text-3xl">
+          Order in 4 simple steps
+        </span>
+      <div className="grid md:grid-cols-2 gap-5">
+        <img src={mobile1Image} />
+        <img src={mobile2Image} />
       </div>
     </div>
   );
